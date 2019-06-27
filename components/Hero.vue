f804366b2432bcfe471a53e252afcdfae7987cbb
S-Chenn <supernovachin@gmail.com>
2019-06-27 22:56:24 +0900

    vue-slickを追加

---
+++ components/Hero.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="hero">
+    <no-ssr>
+
+    </no-ssr>
+  </section>
+</template>
+
+<script>
+import Slick from 'vue-slick'
+import '~/node_modules/slick-carousel/slick/slick.css'
+export default {
+  components: { Slick },
+
+  data() {
+    return {
+      slickOptions: {
+        slidesToShow: 3
+        // Any other options that can be got from plugin documentation
+      }
+    }
+  },
+  methods: {
+    next() {
+      this.$refs.slick.next()
+    },
+    prev() {
+      this.$refs.slick.prev()
+    },
+    reInit() {
+      // Helpful if you have to deal with v-for to update dynamic lists
+      this.$nextTick(() => {
+        this.$refs.slick.reSlick()
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    { src: '~/plugins/vue-slick', ssr: false }
   ],
   /*
   ** Nuxt.js modules
