c0f9c35f743cfb3a9004059b757d94ee29cee994
S-Chenn <supernovachin@gmail.com>
2019-06-27 23:03:21 +0900

    hero仮実装

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,13 @@
 <template>
   <section class="hero">
     <no-ssr>
-
+      <slickref="slick" :is="slickComp" :options="slickOptions" class="slick">
+        <div><img src="@assets/images/hero-1.png" alt="hero-1"></div>
+        <div><img src="@assets/images/hero-2.png" alt="hero-2"></div>
+        <div><img src="@assets/images/hero-3.png" alt="hero-3"></div>
+        <div><img src="@assets/images/hero-4.png" alt="hero-4"></div>
+        <div><img src="@assets/images/hero-5.png" alt="hero-5"></div>
+      </slickref="slick">
     </no-ssr>
   </section>
 </template>
@@ -38,5 +44,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+ .hero {
+   width: 1000px;
+   height: 500px;
+ }
 </style>
