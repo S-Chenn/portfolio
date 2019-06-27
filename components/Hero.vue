3845590f28c5a11a607d86d93927afed9a54a99b
S-Chenn <supernovachin@gmail.com>
2019-06-28 00:23:10 +0900

    仮commit

--- components/Hero.vue
+++ components/Hero.vue
@@ -8,10 +8,11 @@
         :autoplay="true"
         :loop="true"
         :pagination-padding="20"
-        :autoplay-timeout="1000"
+        :autoplay-timeout="5000"
         :speed="1200"
         :navigation-enabled="true"
         :pagination-size="20"
+        :pagination-color="DFE0E1"
       >
         <slide><img src="@/assets/images/hero-1.jpg" class="hero__img hero__img-1" alt="hero-1"></slide>
         <slide><img src="@/assets/images/hero-2.jpg" class="hero__img hero__img-2" alt="hero-2"></slide>
@@ -20,6 +21,12 @@
         <slide><img src="@/assets/images/hero-5.jpg" class="hero__img hero__img-5" alt="hero-5"></slide>
       </carousel>
     </no-ssr>
+    <div class="heading-wrap">
+      <h1 class="heading">
+        Welcome to my Portfolio Site
+      </h1>
+      <div class="dots" />
+    </div>
   </section>
 </template>
 
@@ -36,7 +43,7 @@ export default {
 
 <style lang="scss" scoped>
 .VueCarousel-slide {
-  background: #fff;
+  background: #36393F;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -44,7 +51,28 @@ export default {
   box-sizing: border-box;
 }
  .hero {
+   background-color: #36393F;
+   position: relative;
    width: 100%;
+   .heading-wrap {
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      background-color: rgba(0, 0 ,0, .3);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .heading {
+        font-size: 48px;
+        color: #ffffff;
+        text-align: center;
+        padding: 20%;
+      }
+   }
+   .carousel {
+   }
 
    &__img {
      width: 100%;
