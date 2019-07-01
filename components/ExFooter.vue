89da7acb47a2ea078a9f2abe28fa99d6869526f4
S-Chenn <supernovachin@gmail.com>
2019-07-01 23:22:05 +0900

    footer実装

---
+++ components/ExFooter.vue
@@ -0,0 +1,66 @@
+<template>
+  <footer class="footer">
+    <nav class="footer-nav">
+      <ul class="footer-nav__list">
+        <li class="footer-nav__item">
+          <nuxt-link class="footer-nav__link" to="">
+            Profile
+          </nuxt-link>
+        </li>
+        <li class="footer-nav__item">
+          <nuxt-link class="footer-nav__link" to="">
+            Skills
+          </nuxt-link>
+        </li>
+        <li class="footer-nav__item">
+          <nuxt-link class="footer-nav__link" to="">
+            Contact
+          </nuxt-link>
+        </li>
+      </ul>
+    </nav>
+    <small class="footer__copyright">&copy; S-Chen 2019</small>
+  </footer>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss" scoped>
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 32px;
+  background-color: #FAFAFA;
+  .footer-nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &__list {
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      padding: -16px;
+    }
+    &__item {
+      padding: 16px;
+    }
+    &__link {
+      font-size: 16px;
+      color: #777777;
+      text-decoration: none;
+    }
+  }
+  &__copyright {
+    color: #999999;
+    font-size: 12px;
+  }
+}
+
+.footer-nav__item + .footer-nav__item:before {
+  content: "|";
+  color: #777777;
+  margin: 0 40px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,17 @@
   <div>
     <ex-header />
     <nuxt />
+    <ExFooter />
   </div>
 </template>
 
 <script>
 import ExHeader from '@/components/ExHeader.vue'
+import ExFooter from '@/components/ExFooter.vue'
 export default {
   components: {
-    ExHeader
+    ExHeader,
+    ExFooter
   }
 }
 </script>
