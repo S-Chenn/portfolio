a37e33a7a5412a737d195b2476060d8bafa9a1e0
S-Chenn <supernovachin@gmail.com>
2019-07-02 00:26:23 +0900

    スキルカードコンポーネント作成

---
+++ components/SkillCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <div class="skill">
+      <figure class="skill__icon">
+        <slot />
+      </figure>
+      <h3 class="skill__title">
+        {{ title }}
+      </h3>
+      <p class="skill__description">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['title', 'description']
+}
+</script>
+<style lang="scss">
+
+</style>
