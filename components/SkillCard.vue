9c2b7cca577f845dfd53a153ee663f2f7e081cb9
S-Chenn <supernovachin@gmail.com>
2019-07-02 00:44:09 +0900

    skillcardコンポーネント実装

--- components/SkillCard.vue
+++ components/SkillCard.vue
@@ -19,5 +19,27 @@ export default {
 }
 </script>
 <style lang="scss">
-
+.card {
+  border-radius: 12px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, .12);
+  overflow: hidden;
+}
+.skill {
+  text-align: center;
+  color: #fff;
+  background: linear-gradient(to top right, #000, #2d033a);
+  padding: 24px;
+  height: 100%;
+  &__icon {
+    margin-bottom: 32px;
+  }
+  &__title {
+    margin-bottom: 16px;
+  }
+  &__description {
+    font-size: 13px;
+    color: rgba(255, 255, 255, .6);
+    line-height: 1.8;
+  }
+}
 </style>
