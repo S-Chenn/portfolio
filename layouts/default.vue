33c78a323437c95ed106718568b3e370d03d6a5f
S-Chenn <supernovachin@gmail.com>
2019-07-02 03:33:28 +0900

    headerとfooterが一緒に出てくるdrawer実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <ex-header />
+    <div class="hamburger-menu" @click="drawerOn = !drawerOn" />
+    <div
+      :class="{ jsDrawerMask: drawerOn }"
+      @click="drawerOn = !drawerOn"
+    />
+    <ex-header class="header" :class="{ jsDrawerOpen: drawerOn }" />
     <nuxt />
-    <ExFooter />
+    <ExFooter class="footer" :class="{ jsDrawerOpen: drawerOn }" />
   </div>
 </template>
 
@@ -13,8 +18,56 @@ export default {
   components: {
     ExHeader,
     ExFooter
+  },
+  data() {
+    return {
+      drawerOn: false
+    }
   }
 }
 </script>
-<style>
+<style lang="scss" scoped>
+.header {
+  transform: translateX(-100%);
+  transition: .3s all;
+}
+.footer {
+  transform: translateX(-100%);
+  transition: .3s all;
+}
+.jsDrawerOpen {
+  transform: translateX(0);
+}
+.hamburger-menu {
+  position: fixed;
+  z-index: 10;
+  top: 16px;
+  left: 16px;
+  width: 32px;
+  height: 32px;
+  border-top: 4px solid #000;
+  border-bottom: 4px solid #000;
+  &::before{
+    position: absolute;
+    content: "";
+    height: 10px;
+    width: 100%;
+    border-bottom: 4px solid #000;
+  }
+}
+.jsDrawerMask {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  opacity: 0.6;
+  background-color: #000;
+  z-index: 20;
+}
+@include pc (){
+  .hamburger-menu {
+    display: none;
+  }
+}
 </style>
