93db42f514fbf866dfd0107fa1e2dd0aded9b9bb
S-Chenn <supernovachin@gmail.com>
2019-07-03 03:00:41 +0900

    気持ち悪いwebフォントを実装

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css' },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/earlyaccess/nikukyu.css' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
